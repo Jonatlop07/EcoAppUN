6a077edadc68bce2bff415c29c3288f707a16f72
Jonathan Lopez Castellanos <jolopezca@unal.edu.co>
2023-05-01 17:31:38 -0500

    Moved all the denouncement-related structs to denouncement.go

--- server/denouncements/denouncement.go
+++ server/denouncements/denouncement.go
@@ -14,3 +14,41 @@ type Denouncement struct {
 	MediaLinks  []string  `bson:"mediaLinks" json:"media_links"`
 	Comments    []Comment `bson:"comments" json:"comments"`
 }
+
+type Comment struct {
+	ID             string            `bson:"_id" json:"id"`
+	DenouncementID string            `bson:"denouncementId" json:"denouncement_id"`
+	AuthorID       string            `bson:"authorId" json:"author_id"`
+	Content        string            `bson:"content" json:"content"`
+	CreatedAt      time.Time         `bson:"createdAt" json:"created_at"`
+	Reactions      []CommentReaction `bson:"reactions" json:"reactions"`
+}
+
+type CommentReaction struct {
+	ID             string    `bson:"_id" json:"id"`
+	DenouncementID string    `bson:"denouncementId" json:"denouncement_id"`
+	CommentID      string    `bson:"commentId" json:"comment_id"`
+	Type           string    `bson:"type" json:"type"`
+	UserID         string    `bson:"userId" json:"user_id"`
+	CreatedAt      time.Time `bson:"createdAt" json:"created_at"`
+}
+
+type Response struct {
+	ID             string             `bson:"_id" json:"id"`
+	DenouncementID string             `bson:"denouncementId" json:"denouncement_id"`
+	CommentID      string             `bson:"commentId" json:"comment_id"`
+	AuthorID       string             `bson:"authorId" json:"author_id"`
+	Content        string             `bson:"content" json:"content"`
+	CreatedAt      time.Time          `bson:"createdAt" json:"created_at"`
+	Reactions      []ResponseReaction `bson:"reactions" json:"reactions"`
+}
+
+type ResponseReaction struct {
+	ID             string    `bson:"_id" json:"id"`
+	DenouncementID string    `bson:"denouncementId" json:"denouncement_id"`
+	CommentID      string    `bson:"commentId" json:"comment_id"`
+	ResponseID     string    `bson:"responseId" json:"response_id"`
+	Type           string    `bson:"type" json:"type"`
+	UserID         string    `bson:"userId" json:"user_id"`
+	CreatedAt      time.Time `bson:"createdAt" json:"created_at"`
+}
